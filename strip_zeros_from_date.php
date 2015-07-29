8c1735165f65cca4d35ec67294d7ca4c3827844d
jian <adv@xzerox.org>
2015-07-28 21:47:41 -0700

    init

---
+++ strip_zeros_from_date.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jian Su
+ * Date: 7/6/2015
+ * Time: 9:35 PM
+ */
+
+//$date = date_create('now');
+//echo $now = date_format($date, 'Y-m-d H:i:s')."<br/>";
+
+$timeStamp = time();
+echo $now = strftime("The date today is %m/%d/%y", $timeStamp);
+
+function strip_zeros_from_date($marked_string="") {
+    //remove the marked zeros
+    $no_zeros = str_replace('0', '', $marked_string);
+    return $no_zeros;
+}
+
+echo strip_zeros_from_date($now);
