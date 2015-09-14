ca20988220a023ea522097c50a864e974ed05c9a
jian <adv@xzerox.org>
2015-09-13 21:20:22 -0700

    try to create Transformers but not working, will come back later

---
+++ api/app/Function/Transformers/Transformer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Funciton\Transformers;
+
+abstract class Transformer {
+
+    public function transformCollection(array $items)
+    {
+        return array_map([$this, 'transform'], $items);
+    }
+
+    public abstract function transform($item);
+}
