ca79c16e016922db37b2b13aad547e37e0008e0a
jian <adv@xzerox.org>
2015-09-29 14:21:59 -0700

    init laravel5

---
+++ learning-laravel-5/resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link ref="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+
+</html>
