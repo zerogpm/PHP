6d0581e90865e9b22f55ca259659f80bfddf7c1a
jian <adv@xzerox.org>
2016-01-18 22:10:26 -0800

    change add user route and create UserController

--- api/app/Http/routes.php
+++ api/app/Http/routes.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Route::group(['prefix' => 'api/v1'], function() {
 
     Route::resource('lesson', 'LessonController');
+    Route::resource('User', 'UserController');
 
 });
 
